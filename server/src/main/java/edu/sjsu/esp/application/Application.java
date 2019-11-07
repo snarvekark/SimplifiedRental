9012e05d5066b010ce12c488270fc02092cd4e89
Jahnavi Rangu <55324369+jrangu@users.noreply.github.com>
2019-11-06 20:23:08 -0800

    Server structure for spring boot application

---
+++ server/src/main/java/edu/sjsu/esp/application/Application.java
@@ -0,0 +1,14 @@
+package edu.sjsu.esp.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan({ "edu.sjsu.esp.*" })
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
