6f565f888b0fe05b7c063d1cb1c778dadefca880
Jahnavi Rangu <55324369+jrangu@users.noreply.github.com>
2019-12-03 13:38:22 -0800

    Added customer email column in workorder table.

--- Database/Scripts.sql
+++ Database/Scripts.sql
@@ -49,13 +49,10 @@ DROP TABLE IF EXISTS `technician`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `technician` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(45) NOT NULL,
-  `last_name` varchar(45) NOT NULL,
-  `manager_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_technician_manager` (`manager_id`),
-  CONSTRAINT `FK_technician_manager` FOREIGN KEY (`manager_id`) REFERENCES `manager` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -64,7 +61,7 @@ CREATE TABLE `technician` (
 
 LOCK TABLES `technician` WRITE;
 /*!40000 ALTER TABLE `technician` DISABLE KEYS */;
-INSERT INTO `technician` VALUES (1,'Technician','Mike',1);
+INSERT INTO `technician` VALUES (1,'Mike','Technician'),(2,'Jack','Technician');
 /*!40000 ALTER TABLE `technician` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -77,9 +74,9 @@ DROP TABLE IF EXISTS `user`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(45) NOT NULL,
-  `last_name` varchar(45) NOT NULL,
-  `email` varchar(45) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -90,7 +87,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'Jahnavi','R','jj@gmail.com');
+INSERT INTO `user` VALUES (1,'jahnavi.rangu@sjsu.edu','Jahnavi','Rangu');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -103,23 +100,19 @@ DROP TABLE IF EXISTS `workorder`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `workorder` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `start_date` datetime DEFAULT NULL,
-  `end_date` datetime DEFAULT NULL,
-  `user_id` int(11) NOT NULL,
-  `technician_id` int(11) DEFAULT NULL,
-  `priority` varchar(45) DEFAULT NULL,
+  `apt_no` varchar(255) DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
-  `manager_id` int(11) DEFAULT NULL,
-  `status` varchar(45) NOT NULL,
-  `apt_no` varchar(45) NOT NULL,
+  `end_date` datetime DEFAULT NULL,
+  `priority` varchar(255) DEFAULT NULL,
+  `start_date` datetime DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `technician_id` int(11) DEFAULT NULL,
+  `user_email` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `FK_wo_user` (`user_id`),
-  KEY `FK_wo_technician` (`technician_id`),
-  KEY `FK_wo_manager` (`manager_id`),
-  CONSTRAINT `FK_wo_manager` FOREIGN KEY (`manager_id`) REFERENCES `manager` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `FK_wo_technician` FOREIGN KEY (`technician_id`) REFERENCES `technician` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `FK_wo_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `FK2iww3oftslcro61vouh2gqj4f` (`technician_id`),
+  CONSTRAINT `FK2iww3oftslcro61vouh2gqj4f` FOREIGN KEY (`technician_id`) REFERENCES `technician` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -128,7 +121,7 @@ CREATE TABLE `workorder` (
 
 LOCK TABLES `workorder` WRITE;
 /*!40000 ALTER TABLE `workorder` DISABLE KEYS */;
-INSERT INTO `workorder` VALUES (1,'2019-11-06 23:59:59','2019-11-16 23:59:59',1,NULL,'HIGH','leak in sink',1,'PENDING','10'),(2,'2019-11-06 23:59:59','2019-11-06 23:59:59',1,1,'LOW','work order2',1,'IN PROGRESS','20');
+INSERT INTO `workorder` VALUES (1,'10','workorder1',NULL,'HIGH',NULL,'COMPLETED',1,1,'jahnavi.rangu@sjsu.edu'),(2,'10','workOrder3 Test','2019-11-16 23:59:59','HIGH','2019-11-07 23:59:59','PENDING',1,2,'jahnavi.rangu@sjsu.edu'),(3,NULL,'10 apt, Kitchen',NULL,'High',NULL,NULL,1,1,''),(4,'','',NULL,'High',NULL,NULL,1,1,''),(5,'asdasd','dasdsad',NULL,'High',NULL,'PENDING',1,NULL,'jahnavi.rangu@sjsu.edu'),(6,'as','asa',NULL,'High',NULL,'PENDING',1,NULL,'jahnavi.rangu@sjsu.edu'),(7,'th ','new workorder',NULL,'High',NULL,'PENDING',1,1,'jahnavi.rangu@sjsu.edu'),(8,'10 apt','',NULL,'',NULL,'PENDING',1,NULL,'jahnavi.rangu@sjsu.edu'),(9,'10 apt','wo 3',NULL,'low',NULL,'PENDING',1,1,'jahnavi.rangu@sjsu.edu'),(10,'15 apt','WorkOrder check email',NULL,'high',NULL,'PENDING',NULL,NULL,'jahnavi.rangu@sjsu.edu');
 /*!40000 ALTER TABLE `workorder` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -145,4 +138,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-11-06 21:33:43
+-- Dump completed on 2019-12-03 13:35:53
