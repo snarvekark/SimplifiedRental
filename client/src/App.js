065763bb0f21ea43f6c0f53a6c65bb74f1750112
Jahnavi Rangu <55324369+jrangu@users.noreply.github.com>
2019-12-03 01:16:30 -0800

    Okta front end integration to redirect user to post-login home based on role.

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ import Dashboard from './Dashboard';
 import Manager from './Manager';
 import Technician from './Technician';
 import Navbar from './Navbar';
-import Login from './containers/Login';
+//import Login from './containers/Login';
 import OrderList from './containers/OrderList';
 import MgrOrderList from './containers/MgrOrderList';
 import AssignedTasks from './containers/AssignedTasks';
@@ -21,6 +21,14 @@ import AddTechnician from './containers/AddTechnician';
 import TechOrderList from './containers/TechOrderList';
 import InprogressTasks from './containers/InprogressTasks';
 import Twitter from './containers/Twitter';
+import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
+import Home from './Home';
+import Login from './Login';
+import Protected from './Protected';
+
+function onAuthRequired({history}) {
+	history.push('/login');
+}
 
 class App extends React.Component
 {
@@ -31,20 +39,30 @@ class App extends React.Component
 	          <div>
 			  	<Navbar />
 	            <Switch>
-				  <Route exact path="/"><Dashboard /></Route> 
-				  <Route exact path="/Dashboard"><Dashboard /></Route> 	
-				  <Route exact path="/Manager"><Manager /></Route>
-				  <Route exact path="/Technician"><Technician /></Route>  
-	              <Route exact path="/OrderForm"><OrderForm /></Route>
-	              <Route exact path="/AssignOrder" render={(props) => <AssignOrder {...props} />}><AssignOrder /></Route>
-				  <Route exact path="/Login"><Login /></Route>
-				  <Route exact path="/OrderList"><OrderList /></Route>
-				  <Route exact path="/AddTechnician"><AddTechnician /></Route>
-				  <Route exact path="/AssignedTasks"><AssignedTasks /></Route>
-				  <Route exact path="/TechOrderList"><TechOrderList /></Route>
-				  <Route exact path="/InprogressTasks"><InprogressTasks /></Route>
-				  <Route exact path="/Twitter"><Twitter /></Route>
-				  <Route exact path="/MgrOrderList" render={(props) => <MgrOrderList {...props}/>}><MgrOrderList /></Route>
+				<Security issuer='https://dev-304860.okta.com/oauth2/default'
+                  clientId='0oa20apu12UzBNBYH357'
+                  redirectUri={window.location.origin + '/implicit/callback'}
+                  onAuthRequired={onAuthRequired}
+                  pkce={true} >
+					<Route path='/' exact={true} component={Home} />
+          			<SecureRoute path='/protected' component={Protected} />
+          			<Route path='/login' render={() => <Login baseUrl='https://dev-304860.okta.com/' />} />
+          			<Route path='/implicit/callback' component={ImplicitCallback} />
+					{/* <Route exact path="/"><Dashboard /></Route>  */}
+					<Route exact path="/Dashboard"><Dashboard /></Route> 	
+					<Route exact path="/Manager"><Manager /></Route>
+					<Route exact path="/Technician"><Technician /></Route>  
+					<Route exact path="/OrderForm"><OrderForm /></Route>
+					<Route exact path="/AssignOrder" render={(props) => <AssignOrder {...props} />}><AssignOrder /></Route>
+					{/* <Route exact path="/Login"><Login /></Route> */}
+					<Route exact path="/OrderList"><OrderList /></Route>
+					<Route exact path="/AddTechnician"><AddTechnician /></Route>
+					<Route exact path="/AssignedTasks"><AssignedTasks /></Route>
+					<Route exact path="/TechOrderList"><TechOrderList /></Route>
+					<Route exact path="/InprogressTasks"><InprogressTasks /></Route>
+					<Route exact path="/Twitter"><Twitter /></Route>
+					<Route exact path="/MgrOrderList" render={(props) => <MgrOrderList {...props}/>}><MgrOrderList /></Route>
+				  </Security>
   	          </Switch>
 	          </div>
 	        </Router>
